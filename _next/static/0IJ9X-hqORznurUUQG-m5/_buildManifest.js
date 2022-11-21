5a43d24c7bf14c5b1fb186b0a6d3af0778ff1c26
Farshid Shahmoradi <farshidshahmoradi1996@gmail.com>
2022-11-22 01:35:45 +0330

    sdssd

---
+++ _next/static/0IJ9X-hqORznurUUQG-m5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9f3e5021eb1d11633d6c.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
